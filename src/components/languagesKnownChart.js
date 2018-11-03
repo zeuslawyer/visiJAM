c9a388560202c8de551f8136d0bb9947fa7719b8
Sarah Kaplan <skaplan520@gmail.com>
2018-11-03 15:47:46 -0700

    edited stacked bar chart in Languages known

--- src/components/languagesKnownChart.js
+++ src/components/languagesKnownChart.js
@@ -3,18 +3,47 @@ import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 
 const options = {
-  title: {
-    text: 'My chart'
+  chart: {
+    type: 'bar',
   },
-  series: [{
-    data: [1, 2, 3]
-  }]
+  width: 500,
+  title: {
+    text: 'Programming Languages by Experience Level',
+  },
+  xAxis: {
+    categories: ['Javascript', 'Python', 'Java', 'C++', 'Ruby', 'PHP', 'Other'],
+  },
+  yAxis: {
+    min: 0,
+    title: {
+      text: 'Total JAMstackers using each language',
+    },
+  },
+  legend: {
+    reversed: true,
+  },
+  plotOptions: {
+    series: {
+      stacking: 'normal',
+    },
+  },
+  series: [
+    {
+      name: 'Advanced',
+      data: [5, 3, 4, 7, 2, 10, 2],
+    },
+    {
+      name: 'Intermediate',
+      data: [2, 2, 3, 2, 1, 1, 0],
+    },
+    {
+      name: 'Beginner',
+      data: [3, 4, 4, 2, 5, 9, 5],
+    },
+  ],
 }
 
 const LanguagesKnownChart = () => (
-      <HighchartsReact
-        highcharts={Highcharts}
-        options={options}
-      ></HighchartsReact>
+  <HighchartsReact highcharts={Highcharts} options={options} />
 )
 export default LanguagesKnownChart
