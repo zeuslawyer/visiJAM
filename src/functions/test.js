26d79926326652e141a99a9b48a64452a5b3c7ab
zubin | zeuscoder <zubin.pratap@gmail.com>
2018-12-16 19:13:47 +1100

    reset to previous head.  refactor with new "test" collection. working locally. next->set env variable on netlify and test

--- src/functions/test.js
+++ src/functions/test.js
@@ -6,47 +6,30 @@ const uri = secrets.URI
 
 const database = require('../helpers/db/getDb.js')
 
+exports.handler = async function(event, context, callback) {
+  const fetchedDB = await database
+  // console.log('FETCHED DB IS:   ', fetchedDB)
+  fetchedDB
+    .collection('test')
+    .find({})
+    .toArray()
+    .then(data => {
+      callback(null, { statusCode: 200, body: JSON.stringify(data) })
+    })
+    .catch(err =>
+      console.log('ERRRRRROOOORRRR!!!!!!!!!!>>>>>>   ', err.message)
+    )
+}
+
 // exports.handler = async function(event, context, callback) {
-//   const fetchedDB = await database
-//   // console.log('FETCHED DB IS:   ', fetchedDB)
-//   fetchedDB
+//   let fetched = await database
+
+//   fetched
+//     // .db('visiJAM-DB')
 //     .collection('users')
 //     .find({})
 //     .toArray()
 //     .then(data => {
 //       callback(null, { statusCode: 200, body: JSON.stringify(data) })
 //     })
-//     .catch(err =>
-//       console.log('ERRRRRROOOORRRR!!!!!!!!!!>>>>>>   ', err.message)
-//     )
 // }
-
-exports.handler = async function(event, context, callback) {
-  let fetched = await database
-
-  fetched
-    // .db('visiJAM-DB')
-    .collection('users')
-    .find({})
-    .toArray()
-    .then(data => {
-      callback(null, { statusCode: 200, body: JSON.stringify(data) })
-    })
-}
-
-// const run = async function() {
-//   const fetchedDB = await database
-//   // console.log('FETCHED DB IS:   ', fetchedDB)
-//   fetchedDB
-//     .collection('users')
-//     .find({})
-//     .toArray()
-//     .then(data => {
-//       console.log(JSON.stringify(data))
-//     })
-//     .catch(err =>
-//       console.log('ERRRRRROOOORRRR!!!!!!!!!!>>>>>>   ', err.message)
-//     )
-// }
-
-// run()
